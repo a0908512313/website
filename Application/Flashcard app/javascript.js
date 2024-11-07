05228cf3a4f12c6b3e3da88e1b764f21b3477c2b
Pear <a0908512313@gmail.com>
2024-11-08 02:09:01 +0800

    flash card fix

--- Application/Flashcard app/javascript.js
+++ Application/Flashcard app/javascript.js
@@ -1,11 +1,11 @@
 // Selecting DOM elements
 const container = document.querySelector(".container");
-const addQuestionModal = document.getElementById("add-card-modal");
-const saveBtn = document.getElementById("save-btn");
+const addQuestionCard = document.getElementById("add-question-card");
+const cardButton = document.getElementById("save-btn");
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 const errorMessage = document.getElementById("error");
-const addQuestion = document.getElementById("add-card");
+const addQuestion = document.getElementById("add-flashcard");
 const closeBtn = document.getElementById("close-btn");
 
 // Initializing variables
@@ -14,22 +14,23 @@ let originalId = null;
 let flashcards = JSON.parse(localStorage.getItem("flashcards")) || [];
 
 addQuestion.addEventListener("click", () => {
-	// Show the add question modal and hide the container
+	// Show the add question card and hide the container
 	container.classList.add("hide");
 	question.value = "";
 	answer.value = "";
-	addQuestionModal.classList.remove("hide");
+	addQuestionCard.classList.remove("hide");
 });
 
 closeBtn.addEventListener("click", () => {
-	// Close the add question modal and show the container
+	// Close the add question card and show the container
 	container.classList.remove("hide");
-	addQuestionModal.classList.add("hide");
+	addQuestionCard.classList.add("hide");
 	if (editBool) {
 		editBool = false;
 	}
 });
-saveBtn.addEventListener("click", () => {
+
+cardButton.addEventListener("click", () => {
 	// Save the flashcard
 	let tempQuestion = question.value.trim();
 	let tempAnswer = answer.value.trim();
@@ -46,79 +47,75 @@ saveBtn.addEventListener("click", () => {
 		let id = Date.now();
 		// Add the new flashcard to the array
 		flashcards.push({ id, question: tempQuestion, answer: tempAnswer });
-		// Save the flashcards array to localStorage
+		// Save the flashcards array to local storage
 		localStorage.setItem("flashcards", JSON.stringify(flashcards));
 		container.classList.remove("hide");
 		errorMessage.classList.add("hide");
-		viewList();
+		viewlist();
 		question.value = "";
 		answer.value = "";
 		editBool = false;
-		addQuestionModal.classList.add("hide");
+		addQuestionCard.classList.add("hide"); // This line hides the modal after the flashcard is added
 	}
 });
 
 // Function to display the flashcard list
-function viewList() {
-	const cardList = document.querySelector(".cards-list");
-	cardList.innerHTML = "";
+function viewlist() {
+	const listCard = document.querySelector(".card-list-container");
+	listCard.innerHTML = "";
 	flashcards = JSON.parse(localStorage.getItem("flashcards")) || [];
 	flashcards.forEach((flashcard) => {
 		const div = document.createElement("div");
 		div.classList.add("card");
 		div.innerHTML = `
-		<p class="que-div">${flashcard.question}</p>
-		<p class="ans-card hide">${flashcard.answer}</p>
-		<button class="show-hide-btn">Show/Hide</button>
-		<div class="btns-con">
-			<button class="edit">
-				<i class="fa-solid fa-pen-to-square"></i>
-			</button>
-			<button class="delete">
-				<i class="fa-solid fa-trash"></i>
-			</button>
-		</div>`;
-
+      <p class="question-div">${flashcard.question}</p>
+      <p class="answer-div hide">${flashcard.answer}</p>
+      <a href="#" class="show-hide-btn">Show/Hide</a>
+      <div class="buttons-con">
+        <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="delete"><i class="fa-solid fa-trash-can"></i></button>
+      </div>
+    `;
 		div.setAttribute("data-id", flashcard.id);
-		const dispalyAns = div.querySelector(".ans-div");
+		const displayAnswer = div.querySelector(".answer-div");
 		const showHideBtn = div.querySelector(".show-hide-btn");
-		const editBtn = div.querySelector(".edit");
-		const deleteBtn = div.querySelector(".delete");
+		const editButton = div.querySelector(".edit");
+		const deleteButton = div.querySelector(".delete");
 
 		showHideBtn.addEventListener("click", () => {
 			// Toggle the visibility of the answer
-			dispalyAns.classList.toggle("hide");
+			displayAnswer.classList.toggle("hide");
 		});
 
-		editBtn.addEventListener("click", () => {
+		editButton.addEventListener("click", () => {
 			// Enable editing mode and show the add question card
 			editBool = true;
-			modifyElement(editBtn, true);
-			addQuestionModal.classList.remove("hide");
+			modifyElement(editButton, true);
+			addQuestionCard.classList.remove("hide");
 		});
 
-		deleteBtn.addEventListener("click", () => {
+		deleteButton.addEventListener("click", () => {
 			// Delete the flashcard
-			modifyElement(deleteBtn);
+			modifyElement(deleteButton);
 		});
 
-		cardList.appendChild(div);
+		listCard.appendChild(div);
 	});
 }
 
-// Function to modify a flashcard
+// Function to modify a flashcard element
 const modifyElement = (element, edit = false) => {
-	const parentDiv = document.parentElement.parentElement;
+	const parentDiv = element.parentElement.parentElement;
 	const id = Number(parentDiv.getAttribute("data-id"));
-	const parentQuestion = parentDiv.querySelector(".que-div").innerText;
+	const parentQuestion = parentDiv.querySelector(".question-div").innerText;
 	if (edit) {
-		const parentAnswer = parentDiv.querySelector(".ans-div").innerText;
-		answer.value = parentAnswer;
+		const parentAns = parentDiv.querySelector(".answer-div").innerText;
+		answer.value = parentAns;
 		question.value = parentQuestion;
 		originalId = id;
-		disableBtns(true);
+		disableButtons(true);
 	} else {
-		// Remove the flashcard from array and update localStorage
+		// Remove the flashcard from the array and update local storage
 		flashcards = flashcards.filter((flashcard) => flashcard.id !== id);
 		localStorage.setItem("flashcards", JSON.stringify(flashcards));
 	}
@@ -126,12 +123,12 @@ const modifyElement = (element, edit = false) => {
 };
 
 // Function to disable edit buttons
-const disableBtns = (value) => {
+const disableButtons = (value) => {
 	const editButtons = document.getElementsByClassName("edit");
 	Array.from(editButtons).forEach((element) => {
-		element.disable = true;
+		element.disabled = value;
 	});
 };
 
-// Even listener to display the flashcard list when the DOM is loaded
-document.addEventListener("DOMContentLoaded", viewList());
+// Event listener to display the flashcard list when the DOM is loaded
+document.addEventListener("DOMContentLoaded", viewlist);
